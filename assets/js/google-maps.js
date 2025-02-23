66b31604bca74387fed8256611ae6a2a9789b604
freedevm <kimotohisao17@gmail.com>
2025-02-23 10:56:03 +0900

    update map

--- assets/js/google-maps.js
+++ assets/js/google-maps.js
@@ -1,7 +1,7 @@
 function initMap() {
     // Latitude and Longitude
     // var myLatLng = {lat: -6.207690, lng: 106.985270};
-    var myLatLng = {lat: 42.29493, lng: -84.04053};
+    var myLatLng = {lat: 42.29373, lng: -84.09385};
 
     var map = new google.maps.Map(document.getElementById('google-maps'), {
         zoom: 17,
@@ -11,6 +11,6 @@ function initMap() {
     var marker = new google.maps.Marker({
         position: myLatLng,
         map: map,
-        title: ' 23018 Evergreen Cir, Chelsea, MI 48118, USA' // Title Location
+        title: ' 4030 Kalmbach Rd, Chelsea, MI 48118' // Title Location
     });
 }
